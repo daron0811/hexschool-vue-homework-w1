2a5af744d57e053fcd66c2162c25ed6cf7a62a48
DaRon-HomePC <max99124@gmail.com>
2025-07-31 03:15:14 +0800

    add gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/hexschool-vue-homework-w1/',
   plugins: [
     vue(),
     vueDevTools(),
