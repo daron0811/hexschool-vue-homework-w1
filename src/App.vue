0209f39b7951c003c6c2ec1ed1978e6f3965a494
DaRon-HomePC <max99124@gmail.com>
2025-07-31 01:53:31 +0800

    add : first project

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue';
+
+const item = ref(
+  [
+    { id: Math.random, name: '珍珠奶茶', desc: 90, price: 50, amount: 3 },
+    { id: Math.random, name: '冬瓜檸檬', desc: 85, price: 50, amount: 3 },
+    { id: Math.random, name: '翡翠檸檬', desc: 78, price: 50, amount: 3 },
+    { id: Math.random, name: '四季春茶', desc: 92, price: 50, amount: 3 },
+    { id: Math.random, name: '阿薩姆奶茶', desc: 85, price: 50, amount: 3 },
+    { id: Math.random, name: '檸檬冰茶', desc: 78, price: 50, amount: 3 },
+    { id: Math.random, name: '芒果綠茶', desc: 92, price: 50, amount: 3 },
+    { id: Math.random, name: '抹茶拿鐵', desc: 85, price: 50, amount: 3 }
+  ]
+);
+
+function addItem(item, _add = true) {
+  if (_add) {
+    item.amount++;
+  } else {
+    if (item.amount <= 0)
+      return;
+    item.amount--;
+  }
+}
+
+function resetName(item) {
+  /*
+  //TODO:做空值檢查
+  let promptText = prompt('請輸入新的品項名稱', item.name);
+  console.log(promptText);
+  if (promptText === null) {
+    alert('品項名稱不能為空');
+    return;
+  }
+  item.name = promptText;*/
+  item.name = prompt('請輸入新的品項名稱', item.name);
+}
+
+</script>
+
+<template>
+  <table>
+    <thead>
+      <th scope="col">品項</th>
+      <th scope="col">描述</th>
+      <th scope="col">價格</th>
+      <th scope="col">庫存</th>
+    </thead>
+    <tbody>
+      <tr v-for="(i, index) in item" :key="index">
+        <td>{{ i.name }}</td>
+        <td>{{ i.desc }}</td>
+        <td>{{ i.price }}</td>
+        <td><button v-on:click="addItem(index, false)">-</button>{{ i.amount }}<button v-on:click="addItem(i)">+</button>
+        </td>
+        <td><button v-on:click="resetName(index)">編輯</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<style></style>
---
+++ src/AppPratice.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+const a = ref(2); // 使用ref去做響應式動態更新
+
+function showValue() {
+  alert(`你輸入的是${a.value}`);
+}
+
+const students = ref([
+  { id: 's001', name: '洧杰', score: 90 },
+  { id: 's002', name: '小花', score: 85 },
+  { id: 's003', name: '阿明', score: 78 },
+  { id: 's004', name: '佩佩', score: 92 }
+])
+
+</script>
+
+<template>
+  <input type="text" v-bind:value="a"></input> <!-- v-bind 僅讀取值  文字輸入欄位輸入預設值-->
+  <input type="text" v-model="a"> </input><!-- v-model 雙向綁定 -->
+  <input type="text" v-model="a"> </input><!-- v-model 雙向綁定 -->
+  <h1>{{ a }}</h1>
+  <h1>{{ a }}</h1>
+  <h1>{{ a }}</h1>
+  <h1>{{ a }}</h1>
+  <button v-on:click="a++">點我+1</button>"
+  <button v-on:click="showValue(a)">點我</button>"
+
+  <li v-for="student in students" :key="student.id" class="mb-2 p-2 border rounded">
+    <p>🆔 學號：{{ student.id }}</p>
+    <p>📛 姓名：{{ student.name }}</p>
+    <p>📈 分數：{{ student.score }}</p>
+  </li>
+</template>
+<style></style>
