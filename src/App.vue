ff3f334ae8fdef37fda099b63032fe7a422fe2d4
DaRon-HomePC <max99124@gmail.com>
2025-07-31 19:23:17 +0800

    1.新增gh-page deploy 2.修正部分變數名稱及編輯名稱邏輯

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <script setup>
 import { ref } from 'vue';
 
-const item = ref(
+const items = ref(
   [
-    { id: Math.random, name: '珍珠奶茶', desc: 90, price: 50, amount: 3 },
-    { id: Math.random, name: '冬瓜檸檬', desc: 85, price: 50, amount: 3 },
-    { id: Math.random, name: '翡翠檸檬', desc: 78, price: 50, amount: 3 },
-    { id: Math.random, name: '四季春茶', desc: 92, price: 50, amount: 3 },
-    { id: Math.random, name: '阿薩姆奶茶', desc: 85, price: 50, amount: 3 },
-    { id: Math.random, name: '檸檬冰茶', desc: 78, price: 50, amount: 3 },
-    { id: Math.random, name: '芒果綠茶', desc: 92, price: 50, amount: 3 },
-    { id: Math.random, name: '抹茶拿鐵', desc: 85, price: 50, amount: 3 }
+    { id: Math.random(), name: '珍珠奶茶', desc: `香濃奶茶搭配QQ珍珠!`, price: 10, amount: 3 },
+    { id: Math.random(), name: '冬瓜檸檬', desc: `清新冬瓜配上新鮮檸檬!`, price: 20, amount: 3 },
+    { id: Math.random(), name: '翡翠檸檬', desc: `綠茶與檸檬的完美結合!`, price: 30, amount: 3 },
+    { id: Math.random(), name: '四季春茶', desc: `香醇四季春茶，回甘無比!`, price: 50, amount: 3 },
+    { id: Math.random(), name: '阿薩姆奶茶', desc: `阿薩姆紅茶搭配香醇鮮奶!`, price: 50, amount: 3 },
+    { id: Math.random(), name: '檸檬冰茶', desc: `檸檬與冰茶的清新組合!`, price: 60, amount: 3 },
+    { id: Math.random(), name: '芒果綠茶', desc: `芒果與綠茶的獨特風味!`, price: 70, amount: 3 },
+    { id: Math.random(), name: '抹茶拿鐵', desc: `抹茶與鮮奶的絕配!`, price: 80, amount: 3 }
   ]
 );
+
+const editIdx = ref(0);
+const editName = ref(``);
 
 function addItem(item, _add = true) {
   if (_add) {
     item.amount++;
   } else {
-    if (item.amount <= 0)
+    if (item.amount <= 0) {
       return;
+    }
     item.amount--;
   }
 }
+
 
 function resetName(item) {
   /*
@@ -34,7 +39,19 @@ function resetName(item) {
     return;
   }
   item.name = promptText;*/
-  item.name = prompt('請輸入新的品項名稱', item.name);
+  let promptText = prompt('請輸入新的品項名稱', item.name);
+  // item.name = prompt('請輸入新的品項名稱', item.name);
+  console.log(promptText);
+  item.name = promptText ? promptText : item.name;
+}
+
+function startEditName(item) {
+  editIdx.value = item.id;
+  editName.value = item.name;
+}
+
+function comfirmEdit() {
+  editIdx.value = 0;
 }
 
 </script>
@@ -46,16 +63,33 @@ function resetName(item) {
       <th scope="col">描述</th>
       <th scope="col">價格</th>
       <th scope="col">庫存</th>
+      <th scope="col">更改</th>
     </thead>
     <tbody>
-      <tr v-for="(i, index) in item" :key="index">
-        <td>{{ i.name }}</td>
-        <td>{{ i.desc }}</td>
-        <td>{{ i.price }}</td>
-        <td><button v-on:click="addItem(index, false)">-</button>{{ i.amount }}<button
-            v-on:click="addItem(i)">+</button>
+      <tr v-for="(item, index) in items" :key="index">
+
+        <td v-if="editIdx !== item.id" v-on:click="startEditName(item)">
+          {{ item.name }}
         </td>
-        <td><button v-on:click="resetName(index)">編輯</button></td>
+        <td v-else>
+          <input type="text" v-model="item.name">
+          </input>
+        </td>
+
+        <td>{{ item.desc }}</td>
+        <td>{{ item.price }}</td>
+        <td>
+          <button v-on:click="addItem(item, false)">-</button>
+          {{ item.amount }}
+          <button v-on:click="addItem(item)">+</button>
+        </td>
+
+        <td v-if="editIdx !== item.id">
+          <button v-on:click="startEditName(item)">編輯</button>
+        </td>
+        <td v-else>
+          <button v-on:click="comfirmEdit(item)">確定</button>
+        </td>
       </tr>
     </tbody>
   </table>
